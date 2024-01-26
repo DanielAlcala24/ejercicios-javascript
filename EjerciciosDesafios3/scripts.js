cfdcccccc0625de054361406596c5a0e645a77dc
Daniel Alcalá <danielav24102001@gmail.com>
2024-01-25 22:33:13 -0600

    creamos nuevos desafíos 3

---
+++ EjerciciosDesafios3/scripts.js
@@ -0,0 +1,33 @@
+/*
+
+
+
+Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion en orden inverso.
+Crea una función que calcule el promedio de los elementos en una lista de números.
+Crea una función que muestre en la consola el número más grande y el número más pequeño en una lista.
+Crea una función que devuelva la suma de todos los elementos en una lista.
+Crea una función que devuelva la posición en la lista donde se encuentra un elemento pasado como parámetro, o -1 si no existe en la lista.
+Crea una función que reciba dos listas de números del mismo tamaño y devuelva una nueva lista con la suma de los elementos uno a uno.
+Crea una función que reciba una lista de números y devuelva una nueva lista con el cuadrado de cada número. */
+
+// 1. Crea una lista vacía llamada "listaGenerica".
+
+let listaGenerica = [];
+
+// 2. Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los siguientes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+
+let lenguagesDeProgramacion = ["Javascript", "C", "C++", "Kotlin", "Python"];
+
+// 3. Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby' y 'GoLang'.
+
+lenguagesDeProgramacion.push('Java', 'Ruby', 'GoLang');
+
+// 4. Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion.
+
+function mostrarLista(){
+    for (let i = 0; i < lenguagesDeProgramacion.length; i++){
+        console.log(lenguagesDeProgramacion[i]);
+    }
+}
+    
+mostrarLista();
